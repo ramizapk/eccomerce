6ea7ad7e6b6d96d39064f8d8006096b93497c335
Ramiz Ali <ramiz.apk7@gmail.com>
2026-03-09 01:16:56 +0300

    fix images

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'store.salasahgo.com',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+      },
+      {
+        protocol: 'http',
+        hostname: '127.0.0.1',
+      }
+    ],
+  },
 };
 
 export default nextConfig;
